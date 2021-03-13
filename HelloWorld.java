49a90be131be2c96680a37eb023ea3b607fe4c89
Simone Benedetti <simone.benedetti.416346@alumni.uniroma2.eu>
2021-03-13 10:14:59 +0100

    added file hello world

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.logging.Logger;
+
+public class HelloWorld {
+	
+	 private static final Logger logger1 = Logger.getLogger(HelloWorld.class.getName());
+	 
+	 public static void main(String[] args) {
+		
+		 logger1.info("hello world!");
+
+	}
+
+}
