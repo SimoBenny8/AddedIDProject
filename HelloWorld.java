0f0fe8a4db08e93d571300284e3803637c3c223a
Simone Benedetti <simone.benedetti.416346@alumni.uniroma2.eu>
2021-03-13 10:30:46 +0100

    new name added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,11 @@ import java.util.logging.Logger;
 public class HelloWorld {
 	
 	 private static final Logger logger1 = Logger.getLogger(HelloWorld.class.getName());
-	 
+	 private String name = "Giorgio";
 	 public static void main(String[] args) {
 		
-		 logger1.info("ciao mondo!");
+		 logger1.info("ciao" + name);
+		 	
 
 	}
 
